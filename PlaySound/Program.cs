431c19b4d013905039ea178abf82550b0e246ed4
Victor Martin <victormartin@centralite.com>
2017-11-27 16:52:16 -0600

    Play sounds on timer

--- PlaySound/Program.cs
+++ PlaySound/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Media;
+using System.Threading;
+
 namespace PlaySound
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 2500;
+            timer.Enabled = true;
+
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
+
+
+        }
+
+        private static void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            SystemSounds.Hand.Play();
+            Thread.Sleep(750);
+            SystemSounds.Asterisk.Play();
         }
     }
 }
